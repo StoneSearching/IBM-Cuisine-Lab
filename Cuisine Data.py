344d53a44cb3549bb3b32188981780b29c8bc4bc
Natalie <68333668+StoneSearching@users.noreply.github.com>
2020-07-22 14:01:59 -0400

    Add files via upload

---
+++ Cuisine Data.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+get_ipython().system('python -V')
+
+
+# In[2]:
+
+
+import pandas as pd
+
+
+# In[3]:
+
+
+pd.set_option('display.max_columns', None)
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+import re
+
+
+# In[6]:
+
+
+recipes = pd.read_csv("https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DS0103EN/labs/data/recipes.csv")
+
+print("Data read into dataframe!")
+
+
+# In[7]:
+
+
+recipes.head()
+
+
+# In[8]:
+
+
+recipes.shape
+
+
+# In[9]:
+
+
+ingredients = list(recipes.columns.values)
+
+
+# In[10]:
+
+
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(rice).*")).search(ingredient)] if match])
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(wasabi).*")).search(ingredient)] if match])
+print([match.group(0) for ingredient in ingredients for match in [(re.compile(".*(soy).*")).search(ingredient)] if match])
+
+
+# In[11]:
+
+
+recipes["country"].value_counts()
+
+
+# In[12]:
+
+
+column_names = recipes.columns.values
+column_names[0] = "cuisine"
+recipes.columns = column_names
+
+recipes
+
+
+# In[13]:
+
+
+recipes["cuisine"] = recipes["cuisine"].str.lower()
+
+
+# In[14]:
+
+
+recipes.loc[recipes["cuisine"] == "austria", "cuisine"] = "austrian"
+recipes.loc[recipes["cuisine"] == "belgium", "cuisine"] = "belgian"
+recipes.loc[recipes["cuisine"] == "china", "cuisine"] = "chinese"
+recipes.loc[recipes["cuisine"] == "canada", "cuisine"] = "canadian"
+recipes.loc[recipes["cuisine"] == "netherlands", "cuisine"] = "dutch"
+recipes.loc[recipes["cuisine"] == "france", "cuisine"] = "french"
+recipes.loc[recipes["cuisine"] == "germany", "cuisine"] = "german"
+recipes.loc[recipes["cuisine"] == "india", "cuisine"] = "indian"
+recipes.loc[recipes["cuisine"] == "indonesia", "cuisine"] = "indonesian"
+recipes.loc[recipes["cuisine"] == "iran", "cuisine"] = "iranian"
+recipes.loc[recipes["cuisine"] == "italy", "cuisine"] = "italian"
+recipes.loc[recipes["cuisine"] == "japan", "cuisine"] = "japanese"
+recipes.loc[recipes["cuisine"] == "israel", "cuisine"] = "jewish"
+recipes.loc[recipes["cuisine"] == "korea", "cuisine"] = "korean"
+recipes.loc[recipes["cuisine"] == "lebanon", "cuisine"] = "lebanese"
+recipes.loc[recipes["cuisine"] == "malaysia", "cuisine"] = "malaysian"
+recipes.loc[recipes["cuisine"] == "mexico", "cuisine"] = "mexican"
+recipes.loc[recipes["cuisine"] == "pakistan", "cuisine"] = "pakistani"
+recipes.loc[recipes["cuisine"] == "philippines", "cuisine"] = "philippine"
+recipes.loc[recipes["cuisine"] == "scandinavia", "cuisine"] = "scandinavian"
+recipes.loc[recipes["cuisine"] == "spain", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "portugal", "cuisine"] = "spanish_portuguese"
+recipes.loc[recipes["cuisine"] == "switzerland", "cuisine"] = "swiss"
+recipes.loc[recipes["cuisine"] == "thailand", "cuisine"] = "thai"
+recipes.loc[recipes["cuisine"] == "turkey", "cuisine"] = "turkish"
+recipes.loc[recipes["cuisine"] == "vietnam", "cuisine"] = "vietnamese"
+recipes.loc[recipes["cuisine"] == "uk-and-ireland", "cuisine"] = "uk-and-irish"
+recipes.loc[recipes["cuisine"] == "irish", "cuisine"] = "uk-and-irish"
+
+recipes
+
+
+# In[15]:
+
+
+recipes_counts = recipes["cuisine"].value_counts()
+cuisines_indices = recipes_counts > 50
+
+cuisines_to_keep = list(np.array(recipes_counts.index.values)[np.array(cuisines_indices)])
+
+
+# In[16]:
+
+
+rows_before = recipes.shape[0] # number of rows of original dataframe
+print("Number of rows of original dataframe is {}.".format(rows_before))
+
+recipes = recipes.loc[recipes['cuisine'].isin(cuisines_to_keep)]
+
+rows_after = recipes.shape[0] # number of rows of processed dataframe
+print("Number of rows of processed dataframe is {}.".format(rows_after))
+
+print("{} rows removed!".format(rows_before - rows_after))
+
+
+# In[18]:
+
+
+recipes = recipes.replace(to_replace="Yes", value=1)
+recipes = recipes.replace(to_replace="No", value=0)
+
+
+# In[19]:
+
+
+recipes.head()
+
+
+# In[20]:
+
+
+check_recipes = recipes.loc[
+    (recipes["rice"] == 1) &
+    (recipes["soy_sauce"] == 1) &
+    (recipes["wasabi"] == 1) &
+    (recipes["seaweed"] == 1)
+]
+
+check_recipes
+
+
+# In[21]:
+
+
+ing = recipes.iloc[:, 1:].sum(axis=0)
+
+
+# In[22]:
+
+
+ingredient = pd.Series(ing.index.values, index = np.arange(len(ing)))
+count = pd.Series(list(ing), index = np.arange(len(ing)))
+
+# create the dataframe
+ing_df = pd.DataFrame(dict(ingredient = ingredient, count = count))
+ing_df = ing_df[["ingredient", "count"]]
+print(ing_df.to_string())
+
+
+# In[23]:
+
+
+ing_df.sort_values(["count"], ascending=False, inplace=True)
+ing_df.reset_index(inplace=True, drop=True)
+
+print(ing_df)
+
+
+# In[24]:
+
+
+cuisines = recipes.groupby("cuisine").mean()
+cuisines.head()
+
+
+# In[25]:
+
+
+num_ingredients = 4
+
+
+# In[26]:
+
+
+def print_top_ingredients(row):
+    print(row.name.upper())
+    row_sorted = row.sort_values(ascending=False)*100
+    top_ingredients = list(row_sorted.index.values)[0:num_ingredients]
+    row_sorted = list(row_sorted)[0:num_ingredients]
+
+    for ind, ingredient in enumerate(top_ingredients):
+        print("%s (%d%%)" % (ingredient, row_sorted[ind]), end=' ')
+    print("\n")
+
+
+# In[27]:
+
+
+create_cuisines_profiles = cuisines.apply(print_top_ingredients, axis=1)
+
+
+# In[ ]:
+
+
+
+
